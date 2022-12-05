996f8392914bd2bb33c3c94fe50a7e6d26ff2dce
clint911 <clingyking007@gmail.com>
2022-12-05 06:08:40 +0300

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json
    	modified:   src/Minter.js
    	modified:   yarn.lock
    
    src/utils

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,24 +1,55 @@
 import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, MintNFT } from "./utils/interact";
+
 
 const Minter = (props) => {
 
   //State variables
-  const [walletAddress, setWallet] = useState("");
-  const [status, setStatus] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [url, setURL] = useState("");
- 
+  const [walletAddress, setWallet] = useState("");//storing the user address 
+  const [status, setStatus] = useState("");//text to be displayed inside of the button 
+  const [name, setName] = useState("");//stores the name of the nft 
+  const [description, setDescription] = useState("");//stores the description of the nft 
+  const [url, setURL] = useState("");//link to the nft's digital asset 
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0])
+          setStatus("Write a message in the text-field above")
+        } else {
+          setWallet("")
+          setStatus("Connect to MetaMask using the top right button.")
+        }
+      })
+    } else {
+      setStatus(
+        <p>{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install MetaMask, a virtual Ethereum wallet, in your browser.
+          </a>
+        </p>
+      )
+    }
+  }
+
   useEffect(async () => { //TODO: implement
-    
+    const { address, status } = await getCurrentWalletConnected()
+    setWallet(address)
+    setStatus(status)
+
+    addWalletListener()
   }, []);
 
   const connectWalletPressed = async () => { //TODO: implement
-   
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status)
+    setStatus(walletResponse.address)
   };
 
   const onMintPressed = async () => { //TODO: implement
-    
+    const { status } = await MintNFT(url, name, description)
+    setStatus(status)
   };
 
   return (
